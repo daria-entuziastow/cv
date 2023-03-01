131dab9d2867cd4ef516f1bc73e258c9866c5a00
Darya <120902724+daria-entuziastow@users.noreply.github.com>
2023-03-01 14:46:44 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+
+const button = document.querySelector('button');
+
+
+button.addEventListener('click', function() {
+	alert('6 часов дебаггинга могут спасти вас от 5 минут чтения документации 😌\n                                                                - Конфуций')
+})
